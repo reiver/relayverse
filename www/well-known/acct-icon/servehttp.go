72bedb4faf570e33a1205a5c3291d332dbfd2ab4
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-02-05 06:31:30 -0800

    refactor

--- www/well-known/acct-icon/servehttp.go
+++ www/well-known/acct-icon/servehttp.go
@@ -1,20 +1,17 @@
 package verboten
 
 import (
-	"io"
+	"encoding/json"
 	"strings"
 	"net/http"
 	"net/url"
-	"time"
 
-	"github.com/reiver/relayverse/cfg"
-	"github.com/reiver/relayverse/lib/http"
 	"github.com/reiver/relayverse/srv/cache"
 	"github.com/reiver/relayverse/srv/http"
 	"github.com/reiver/relayverse/srv/log"
 )
 
-const path string = "/.well-known/acct-cache"
+const path string = "/.well-known/acct-icon"
 
 func init() {
 	err := httpsrv.Mux.HandlePath(http.HandlerFunc(serveHTTP), path)
@@ -66,13 +63,13 @@ func serveHTTP(responsewriter http.ResponseWriter, request *http.Request) {
 		if !found {
 			const code int = http.StatusBadRequest
 			http.Error(responsewriter, http.StatusText(code), code)
-			log.Error("resource not provided in acct-cache request")
+			log.Error("resource not provided in acct-icon request")
 			return
 		}
 		if 1 != len(resources) {
 			const code int = http.StatusBadRequest
 			http.Error(responsewriter, http.StatusText(code), code)
-			log.Error("too many resources provided in acct-cache request")
+			log.Error("too many resources provided in acct-icon request")
 			return
 		}
 
@@ -84,9 +81,6 @@ func serveHTTP(responsewriter http.ResponseWriter, request *http.Request) {
 	switch request.Method {
 	case http.MethodGet:
 		serveGET(responsewriter, request, resource)
-		return
-	case http.MethodPut:
-		servePUT(responsewriter, request, resource)
 		return
 	default:
 		const code int = http.StatusMethodNotAllowed
@@ -136,85 +130,32 @@ func serveGET(responsewriter http.ResponseWriter, request *http.Request, resourc
 	}
 	log.Debugf("body length: %d", len(body))
 
+	type Activity struct {
+		Icon struct{
+			URL string `json:"url"`
+		} `json:"icon"`
+	}
+	var activity Activity
 	{
-		responsewriter.Header().Add("Content-Type", libhttp.DetectContentType(body))
-
-		_, err := responsewriter.Write(body)
+		err := json.Unmarshal(body, &activity)
 		if nil != err {
-			log.Errorf("problem writing body: %s", err)
-			return
-		}
-	}
-}
-
-func servePUT(responsewriter http.ResponseWriter, request *http.Request, resource string) {
-
-	log := logsrv.Prefix("www("+path+").PUT").Begin()
-	defer log.End()
-
-	if nil == responsewriter {
-		log.Error("nil response-writer")
-		return
-	}
-	if nil == request {
-		const code int = http.StatusInternalServerError
-		http.Error(responsewriter, http.StatusText(code), code)
-		log.Error("nil http-request")
-		return
-	}
-	if nil == request.Header {
-		const code int = http.StatusInternalServerError
-		http.Error(responsewriter, http.StatusText(code), code)
-		log.Error("nil http-request-headers")
-		return
-	}
-	if nil == request.Body {
-		const code int = http.StatusInternalServerError
-		http.Error(responsewriter, http.StatusText(code), code)
-		log.Error("nil http-request-body")
-		return
-	}
-
-	
-	//@TODO: authorization check
-	
-
-	var duration time.Duration = cfg.CacheDurationMinimum()
-
-	var until time.Time = time.Now().Add(duration)
-	{
-		const name string = "Expires"
-
-		var expiresString string = request.Header.Get(name)
-
-		expires, err := http.ParseTime(expiresString)
-		if nil != err {
-			log.Debugf("bad %q HTTP request header value (%q): %s", name, expiresString, err)
-		} else {
-			if 0 < until.Compare(expires) {
-				until = expires
-			}
-		}
-	}
-
-	var body []byte
-	{
-		var err error
-		body, err = io.ReadAll(request.Body)
-		if nil != err {
-			const code int = http.StatusInternalServerError
+			const code int = http.StatusNotFound
 			http.Error(responsewriter, http.StatusText(code), code)
-			log.Errorf("problem reading all of http-request-body")
+			log.Errorf("problem decoding JSON: %s", err)
 			return
 		}
 	}
-	log.Debugf("body length: %d", len(body))
 
-	{
-		cachesrv.Set(resource, body, until)
+	var url string = activity.Icon.URL
+	if "" == url {
+		log.Debugf("BODY: %s", body)
 	}
+		
+//@TODO: validate URL
+		
+	log.Debugf("icon-url: %q", url)
 
 	{
-		libhttp.NoContent(responsewriter, request)
+		http.Redirect(responsewriter, request, url, http.StatusTemporaryRedirect)
 	}
 }
--- www/well-known/imports.go
+++ www/well-known/imports.go
@@ -2,4 +2,6 @@ package verboten
 
 import (
 	_ "github.com/reiver/relayverse/www/well-known/acct-cache"
+	_ "github.com/reiver/relayverse/www/well-known/acct-icon"
+	_ "github.com/reiver/relayverse/www/well-known/acct-image"
 )
